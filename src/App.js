0da29c96d347b51306a2c4b8fa894b940a188eaf
Dastageercz <dastageer.sayed@codezeros.com>
2022-08-02 18:46:17 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -48,6 +48,8 @@ function App() {
   const launchIPO = () => {
       // <Navigate to="/launchico"
   }
+
+
  
   return (
     <div className="App">
@@ -57,10 +59,9 @@ function App() {
       <div className="appBody">
         <div className="icoList">
           <h2>Listed Coins</h2>
-          {/* <ICOlist tokenSymbol="SAGA" tokenName="SAGA" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="100000" />
+          <ICOlist tokenSymbol="SAGA" tokenName="SAGA" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="100000" />
           <ICOlist  tokenSymbol="ABC" tokenName="ABC" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="10000000"/>
-          <ICOlist /> */}
-          {List ? (<h4 className="icoNot">No ICO available</h4> ) : (<div></div>)}
+          <ICOlist />
         </div>
       </div>
       <div className="footer">
@@ -70,7 +71,7 @@ function App() {
         modal
         contentStyle={{ padding: '0px', border: 'none', borderBlockColor: "red" }}
         >
-          <LaunchICO />
+        <LaunchICO />
       </Popup>
       </div>
 

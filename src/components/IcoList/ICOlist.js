016aa311fe96cac85e9fa9a8dedcb1ca00eff7da
Anurag Chemban <anurag.chemban@codezeros.com>
2022-08-03 18:59:28 +0530

    launch ICO

--- src/components/IcoList/ICOlist.js
+++ src/components/IcoList/ICOlist.js
@@ -3,17 +3,17 @@ import Popup from 'reactjs-popup'
 import CoinDetails from '../CoinDetails/CoinDetails'
 import './ICOlist.css'
 
-const ICOlist = ({tokenName, tokenSymbol, tokenAddress, tokenSupply }) => {
+const ICOlist = ({tokenName, tokenSymbol, tokenAddress, tokenSupply, tokenStartDate, tokenEndDate, tokenRate }) => {
   //  {console.log(tokenSymbol[0])} 
   return (
     <div className="list">
-        <div className="img"></div>
+        <div className="img">{tokenSymbol[0]}</div>
         <h4>{tokenSymbol}</h4>
         <h4>{tokenName}</h4>
         <h4>{tokenAddress}</h4>
         <h4>{tokenSupply}</h4>
         <Popup trigger={<button className="detailsButton">View Details</button>} modal>
-          <div><CoinDetails tokenName={tokenName} tokenSymbol={tokenSymbol} tokenAddress={tokenAddress} tokenSupply={tokenSupply} /></div>
+          <div><CoinDetails tokenName={tokenName} tokenSymbol={tokenSymbol} tokenAddress={tokenAddress} tokenSupply={tokenSupply} tokenStartDate={tokenStartDate} tokenEndDate={tokenEndDate} tokenRate={tokenRate} /></div>
         </Popup>
     </div>
   )
